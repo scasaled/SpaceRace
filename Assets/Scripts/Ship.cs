a1ad20116d004ab656bc86b14617712eb8e29c97
scasaled <scasaled@gmail.com>
2016-11-14 18:01:36 +0100

    Levitation

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,43 +4,43 @@ using System.Collections;
 public class Ship : MonoBehaviour
 {
 
-    public float speed = 5.0f;
+    public float speed;
+	public float rotationSpeed;
 
     private float maxRot = 15f;
+	private Rigidbody rb;
 
     // Use this for initialization
     void Start()
     {
-
+		rb = gameObject.GetComponent<Rigidbody> ();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        float angle = ClampAngle(gameObject.transform.eulerAngles.z, -maxRot, maxRot);
-        Debug.Log(angle);
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            gameObject.transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
-            if (angle > -maxRot) gameObject.transform.Rotate(0.0f, 0.0f, -speed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            gameObject.transform.Translate(-speed * Time.deltaTime, 0.0f, 0.0f);
-            if (angle < maxRot) gameObject.transform.Rotate(0.0f, 0.0f, speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-            gameObject.transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+		float angle = ClampAngle(gameObject.transform.rotation.z, -maxRot, maxRot);
+		print(rb.transform.rotation.y);
+		if (Input.GetKey (KeyCode.RightArrow) == Input.GetKey (KeyCode.LeftArrow))
+			rb.angularVelocity = new Vector3 (0.0f, 0.0f, 0.0f);
+		else {	
+			if (Input.GetKey (KeyCode.RightArrow))
+				rb.angularVelocity = new Vector3 (0.0f, rotationSpeed * Time.deltaTime, 0.0f);
+			else if (Input.GetKey (KeyCode.LeftArrow))
+				rb.angularVelocity = new Vector3 (0.0f, -rotationSpeed * Time.deltaTime, 0.0f);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			rb.velocity += transform.forward * speed;
+			//gameObject.transform.Translate (0.0f, 0.0f, speed * Time.deltaTime);
+		}
     }
 
     float ClampAngle(float angle, float min, float max)
     {
-
         if (angle < 90 || angle > 270)
-        {       // if angle in the critic region...
-            if (angle > 180) angle -= 360;  // convert all angles to -180..+180
+        {
+            if (angle > 180) angle -= 360; 
             if (max > 180) max -= 360;
             if (min > 180) min -= 360;
         }
