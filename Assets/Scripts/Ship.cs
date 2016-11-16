9f1a600f994102d66c0622141045f91e723716fb
scasaled <scasaled@gmail.com>
2016-11-16 11:29:14 +0100

    Tornar a la rotacio original (Lerp)

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,47 +4,34 @@ using System.Collections;
 public class Ship : MonoBehaviour
 {
 
-    public float speed;
+	public float speed;
 	public float rotationSpeed;
 
-    private float maxRot = 15f;
+	private float maxRot = 15f;
 	private Rigidbody rb;
+	private Quaternion originalRotation;
 
-    // Use this for initialization
-    void Start()
-    {
+	private float lastTime;
+
+	// Use this for initialization
+	void Start ()
+	{
 		rb = gameObject.GetComponent<Rigidbody> ();
-    }
+		originalRotation = rb.rotation;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-		float angle = ClampAngle(gameObject.transform.rotation.z, -maxRot, maxRot);
-		print(rb.transform.rotation.y);
-		if (Input.GetKey (KeyCode.RightArrow) == Input.GetKey (KeyCode.LeftArrow))
-			rb.angularVelocity = new Vector3 (0.0f, 0.0f, 0.0f);
-		else {	
-			if (Input.GetKey (KeyCode.RightArrow))
-				rb.angularVelocity = new Vector3 (0.0f, rotationSpeed * Time.deltaTime, 0.0f);
-			else if (Input.GetKey (KeyCode.LeftArrow))
-				rb.angularVelocity = new Vector3 (0.0f, -rotationSpeed * Time.deltaTime, 0.0f);
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.RightArrow) == Input.GetKey (KeyCode.LeftArrow)) {
+			rb.rotation = Quaternion.Lerp(rb.rotation, 
+				new Quaternion (originalRotation.x, rb.rotation.y, originalRotation.z, originalRotation.w), 
+				Time.deltaTime * rotationSpeed * 0.1f);
+		} else {
+			float signe = Input.GetKey (KeyCode.RightArrow) ? 1 : -1;	
+			rb.angularVelocity = new Vector3 (0.0f, signe*rotationSpeed * Time.deltaTime, -(signe)*rotationSpeed * Time.deltaTime);
 		}
-		if (Input.GetKey (KeyCode.UpArrow)) {
+		if (Input.GetKey (KeyCode.UpArrow))
 			rb.velocity += transform.forward * speed;
-			//gameObject.transform.Translate (0.0f, 0.0f, speed * Time.deltaTime);
-		}
-    }
-
-    float ClampAngle(float angle, float min, float max)
-    {
-        if (angle < 90 || angle > 270)
-        {
-            if (angle > 180) angle -= 360; 
-            if (max > 180) max -= 360;
-            if (min > 180) min -= 360;
-        }
-        angle = Mathf.Clamp(angle, min, max);
-        return angle;
-    }
+	}
 }
