c92a49364bd824bfac96dfd9cd8ad9100400fdad
scasaled <scasaled@gmail.com>
2016-12-14 20:16:10 +0100

    Menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,15 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     public Menu CurrentMenu;
     public Menu MapSelectionMenu;
 
+    public List<Ship> ships = new List<Ship>();
+    public List<Transform> bars = new List<Transform>();
+
+    private struct bar
+    {
+        public Text text;
+        public Transform transfromBar;
+        public bar(Text _text, Transform _transform)
+        {
+            text = _text;
+            transfromBar = _transform;
+        }
+    }
+
+    private struct props
+    {
+        public float maxSpeed, acceleration, rotationSpeed, grip;
+
+    }
+
+    private props maxs;
+    private List<bar> barsInfo;
+
     // Use this for initialization
     void Start()
     {
+        barsInfo = new List<bar>();
+        foreach (Transform tr in bars)
+            barsInfo.Add(
+                new bar(tr.FindChild("Text").GetComponent<Text>(),
+                tr.FindChild("Bar").GetComponent<Transform>())
+                );
+
+        maxs = new props();
+
+        maxs.maxSpeed = 156f;
+
+        foreach (Ship ship in ships)
+        {
+            if (maxs.acceleration < ship.acceleration) maxs.acceleration = ship.acceleration;
+            if (maxs.rotationSpeed < ship.rotationSpeed) maxs.rotationSpeed = ship.rotationSpeed;
+            if (maxs.grip < ship.maneig) maxs.grip = ship.maneig;
+        }
         ShowMenu(CurrentMenu);
     }
 
@@ -24,7 +66,22 @@ public class MenuManager : MonoBehaviour
 
     public void ShipSelection(int ship)
     {
-        ShowMenu(MapSelectionMenu);
+        float speed = ((ship == 1) ? 136f : 156f);
+        float ratio = Mathf.Max(0, speed / maxs.maxSpeed);
+        barsInfo[0].text.text = speed.ToString("0");
+        barsInfo[0].transfromBar.localScale = new Vector3(ratio, 1, 1);
+
+        ratio = Mathf.Max(0, ships[ship - 1].acceleration / maxs.acceleration);
+        barsInfo[1].text.text = ships[ship - 1].acceleration.ToString();
+        barsInfo[1].transfromBar.localScale = new Vector3(ratio, 1, 1);
+
+        ratio = Mathf.Max(0, ships[ship - 1].rotationSpeed / maxs.rotationSpeed);
+        barsInfo[2].text.text = ships[ship - 1].rotationSpeed.ToString();
+        barsInfo[2].transfromBar.localScale = new Vector3(ratio, 1, 1);
+
+        ratio = Mathf.Max(0, ships[ship - 1].maneig / maxs.grip);
+        barsInfo[3].text.text = ships[ship - 1].maneig.ToString();
+        barsInfo[3].transfromBar.localScale = new Vector3(ratio, 1, 1);
     }
 
     public void MapSelection(int map)
